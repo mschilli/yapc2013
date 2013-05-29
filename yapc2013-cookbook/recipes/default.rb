b42d5c6f53b5225a7b488d4d5fe376b9eb5bb801
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-05-28 23:24:42 -0700

    committing accidentally skipped files

--- yapc2013-cookbook/recipes/default.rb
+++ yapc2013-cookbook/recipes/default.rb
@@ -8,3 +8,33 @@
 #
 
 include_recipe "perl"
+
+cpan_module "Test::More" do
+  force true
+end
+
+cpan_module "Plack" do
+  force true
+end
+
+cpan_module "Dancer" do
+  force true
+end
+
+cpan_module "YAML" do
+  force true
+end
+
+cpan_module "Starman" do
+  force true
+end
+
+psgi_application 'psgi Starman application' do
+  server              'Starman'
+  application_user    'nobody'
+  application_home    '/tmp/examples/Web-App'
+  script              '/tmp/examples/Web-App/bin/app.pl'
+  socket              ':8080'
+  nproc               '2'
+  action              'install'
+end
--- yapc2013-cookbook/test/integration/default/bats/runtest.bats
+++ yapc2013-cookbook/test/integration/default/bats/runtest.bats
@@ -1,5 +1,14 @@
 #!/usr/bin/env bats
 
+@test "restart web app" {
+  pid=`sudo status app | awk '{print $4}'`
+  if [ $pid ]
+  then
+    sudo stop app
+  fi
+  sudo start app
+}
+
 @test "creating Makefile from Makefile.PL" {
   cd /tmp/examples/test/ && perl Makefile.PL
 }
